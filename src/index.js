a1c26cb991b9515f7c670b2c6e7c5bfeb8b8bfca
bluysky <50596811+bluysky@users.noreply.github.com>
2025-03-20 15:14:02 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import MoldManager from './components/MoldManager'; // MoldManager 컴포넌트 임포트
 import reportWebVitals from './reportWebVitals';
-
-// 전역 오류 핸들러
-window.onerror = function (message, source, lineno, colno, error) {
-  console.error('전역 오류 발생:', message, source, lineno, colno, error);
-  // 오류 로깅 서비스에 오류 전송 (예: Sentry, LogRocket)
-  // ...
-  return false; // 브라우저의 기본 오류 처리 방지
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MoldManager /> {/* MoldManager 컴포넌트 렌더링 */}
   </React.StrictMode>
 );
 
